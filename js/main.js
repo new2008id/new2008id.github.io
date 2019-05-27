4554abefb97f804429a73984731b832729a63740
new2008id <33938844+new2008id@users.noreply.github.com>
2019-05-27 14:51:05 +0300

    Сделал делегирование.

--- js/main.js
+++ js/main.js
@@ -13,6 +13,15 @@ let menu = (function (options) {
   let addListeners = function () {
     buttonOpen.addEventListener('click', _toggleMenu);
 
+    /* Делегирование */  
+  
+    menu.addEventListener('click', function(e) {
+      target = e.target;
+      if(target.classList.contains('nav__link')) {
+        _toggleMenu();
+      }
+    });
+    
     let link = document.getElementById('link');
     
     if (link) {
@@ -34,6 +43,44 @@ let menu = (function (options) {
   });
 
 menu.open();
+
+/* Dropdown */
+
+let composition = (function (optionsOne) {
+  let linkOpen = document.querySelector(optionsOne.linkOpen);
+  let linkClose = document.querySelector(optionsOne.linkClose);
+  let composition = document.querySelector(optionsOne.composition);
+
+  let _toggleComposition = function (e) {
+    e.preventDefault();
+    composition.classList.toggle('composition__active');
+  }
+
+  let addListenersTwo = function () {
+    linkOpen.addEventListener('mouseenter', _toggleComposition);
+
+    if (linkTwo) {
+      let linkTwo = document.getElementById('linkTwo');
+
+      linkTwo.addEventListener('mouseleave', _toggleComposition);
+
+      if (linkClose) {
+        linkClose.addEventListener('mouseleave', _toggleComposition);
+      }
+    }
+
+  }
+
+  return {
+    openDropdown: addListenersTwo
+  };
+})
+  ({
+    linkOpen: '#linkTwo',
+    composition: '#composition__open'
+  });
+
+composition.openDropdown();
 
 // /* Dropdown */
 
